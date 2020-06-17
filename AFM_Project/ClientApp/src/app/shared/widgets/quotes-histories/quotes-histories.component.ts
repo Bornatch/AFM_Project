3add47c945bc3e8fdcf959289ad47bafd3178475
Bornatch <valentin.bornatici@gmail.com>
2020-06-17 18:16:32 +0200

    Update quotes-histories.component.ts

--- AFM_Project/ClientApp/src/app/shared/widgets/quotes-histories/quotes-histories.component.ts
+++ AFM_Project/ClientApp/src/app/shared/widgets/quotes-histories/quotes-histories.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import * as Highcharts from 'highcharts/highstock';
+import * as Highcharts from 'highcharts';
+import HC_stock from 'highcharts/modules/stock';
+HC_stock(Highcharts);
 import { ApiService } from '../../../api.service'
 
 @Component({
@@ -8,18 +10,75 @@ import { ApiService } from '../../../api.service'
   templateUrl: './quotes-histories.component.html',
   styleUrls: ['./quotes-histories.component.scss']
 })
-export class QuotesHistoriesComponent implements OnInit {
+export class QuotesHistoriesComponent {
 
   chartOptions: {};
 
   Highcharts = Highcharts;
-  public chart: any;
+   
   quotes: IQuotes[];
   datas = new Array<any>();
 
   constructor(private apiService: ApiService) {
+    this.apiService.getQuotesHistories().subscribe(result => {
+      this.quotes = result;
+      this.setData();
+      this.createCharts();
+    }, error => console.error(error));
   }
 
+
+
+  createCharts() {
+    this.Highcharts.stockChart({
+      chart: {
+        type: 'line'
+      },
+      title: {
+        text: 'Action course'
+      },
+      xAxis: {
+        type: 'category',
+        labels: {
+          rotation: -45,
+          style: {
+            fontSize: '13px',
+            fontFamily: 'Verdana, sans-serif'
+          }
+        }
+      },
+      yAxis: {
+        min: 0,
+        title: {
+          text: 'price'
+        }
+      },
+      legend: {
+        enabled: false
+      },
+      tooltip: {
+        pointFormat: 'Price: <b>{point.y:.2f}</b>'
+      },
+
+      series: [{
+        type: 'line',
+        name: 'AMZN StockPrice',
+        data: this.datas,
+        marker: {
+          enabled: true,
+          radius: 3
+        },
+        shadow: true,
+        tooltip: {
+          valueDecimals: 2
+        },
+        turboThreshold: 25000
+      }]
+
+    });
+
+  }
+  /*
   ngOnInit() {
     this.apiService.getQuotesHistories().subscribe(result => {
       this.quotes = result;
@@ -83,13 +142,10 @@ export class QuotesHistoriesComponent implements OnInit {
             valueDecimals: 2
           },
           turboThreshold: 25000
-        }]*/
+        }]
 
       };   
-    }, error => console.error(error));
-  }
-
-    
+    }, error => console.error(error));*/
 
   setData() {
 
@@ -102,13 +158,10 @@ export class QuotesHistoriesComponent implements OnInit {
         date,
         //this.quotes[i].quoteDateTime,
         this.quotes[i].lastTrade]
-      );
-            
+      );            
     };
-    console.log(this.datas);
-    
+    console.log(this.datas);    
   }
-
 }
 
 interface IQuotes {
